cf107e888c3cbc5157ec9ba0def9898940724822
Sk Mohammadul Haque <mohammadul@gmail.com>
2013-09-18 09:13:36 +0530

    Included tab

--- mattext.c
+++ mattext.c
@@ -63,7 +63,7 @@ int mat_go_next_word(FILEPOINTER fp)
     char ch = 0;
     while((flag < 2) && ((ch = (char)getc(fp))!= EOF))
     {
-        if ((ch =='\v') || (ch =='\r') || (ch =='\n') || isspace(ch) || (ch == ',') || (ch == '!') || (ch == '(') || (ch == ')') || (ch == '{') || (ch == '}') || (ch == '[') || (ch == ']'))
+        if ((ch =='\v') || (ch =='\r') || (ch =='\n') || (ch =='\t') || isspace(ch) || (ch == ',') || (ch == '!') || (ch == '(') || (ch == ')') || (ch == '{') || (ch == '}') || (ch == '[') || (ch == ']'))
         {
             if(flag == 0) flag = 1;
         }
@@ -94,7 +94,7 @@ int mat_count_words_in_line(FILEPOINTER fp, int *count)
             if(flag == -1) flag = 3;/*  line included to handle empty line */
             else flag = 2;
         }
-        else if (isspace(ch) || (ch == ',') || (ch == '!') || (ch == '(') || (ch == ')') || (ch == '{') || (ch == '}') || (ch == '[') || (ch == ']'))
+        else if (isspace(ch) || (ch =='\t') || (ch == ',') || (ch == '!') || (ch == '(') || (ch == ')') || (ch == '{') || (ch == '}') || (ch == '[') || (ch == ']'))
         {
             if(flag == 0)/* changed from  <=0 to ==0 to skip initial space */
             {
@@ -178,7 +178,7 @@ int mat_read_word(FILEPOINTER fp, char *c_word)
     char ch = 0;
     while((flag < 3) && ((ch = (char)getc(fp))!= EOF))/*no need for state 3 to be corrected*/
     {
-        if ((ch =='\v') || (ch =='\r') || (ch =='\n') || isspace(ch) || (ch == ',') || (ch == '!') || (ch == '(') || (ch == ')') || (ch == '{') || (ch == '}') || (ch == '[') || (ch == ']'))
+        if ((ch =='\v') || (ch =='\r') || (ch =='\n') || (ch =='\t') || isspace(ch) || (ch == ',') || (ch == '!') || (ch == '(') || (ch == ')') || (ch == '{') || (ch == '}') || (ch == '[') || (ch == ']'))
         {
             if(flag != 0) flag = 2;
         }
