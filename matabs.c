2d0945be49749cccfe8b4beaa90182a303593273
Sk Mohammadul Haque <mohammadul@gmail.com>
2013-10-18 22:42:21 +0530

    Update matabs.c

--- matabs.c
+++ matabs.c
@@ -4,17 +4,17 @@
 
 MATRIX mat_abs(MATRIX A)
 {
-    int	i, j, m, n;
+    int i, j, m, n;
     MATRIX B;
     m = MatCol(A);
     n = MatRow(A);
 
-    if ((B = mat_creat( n, m, UNDEFINED )) == NULL)
+    if((B = mat_creat( n, m, UNDEFINED )) == NULL)
         return (NULL);
 
     #pragma omp parallel for private(j)
-    for (i=0; i<n; ++i)
-        for (j=0; j<m; ++j)
+    for(i=0; i<n; ++i)
+        for(j=0; j<m; ++j)
         {
             B[i][j] = (mtype)fabs(A[i][j]);
         }
