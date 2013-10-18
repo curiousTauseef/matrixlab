190557bd41e84dbc98844af55026a0b9146018b2
Sk Mohammadul Haque <mohammadul@gmail.com>
2013-10-18 21:59:13 +0530

    Update matfit.c

--- matfit.c
+++ matfit.c
@@ -12,7 +12,7 @@ MATRIX mat_linear_ls_fit(MATRIX data, MATRIX Y, int degree)
     MATRIX A, X;
     n = MatRow(data);
     A = mat_creat(n, degree + 1, ONES_MATRIX);
-    for (i = 0; i<n; ++i)
+    for(i=0; i<n; ++i)
     {
         for (j = degree-1; j>=0; --j) A[i][j] = data[i][0]*A[i][j+1];
     }
@@ -27,15 +27,15 @@ MATRIX mat_least_squares(MATRIX A, MATRIX Y, MATRIX result)
     MATRIX Apinv;
     if(MatRow(A)!=MatRow(Y)) return mat_error(MAT_SIZEMISMATCH);
     Apinv = mat_pinv(A, NULL);
-    if (Apinv ==NULL) return mat_error(MAT_INVERSE_ILL_COND);
+    if(Apinv ==NULL) return mat_error(MAT_INVERSE_ILL_COND);
     m = MatCol(Apinv);
     n = MatRow(Apinv);
     o = MatCol(Y);
-    if (result == NULL) if ((result= mat_creat( n, o, UNDEFINED )) == NULL)
+    if(result == NULL) if ((result= mat_creat( n, o, UNDEFINED )) == NULL)
             return mat_error(MAT_MALLOC);
-    for (i=0; i<n; ++i)
-        for (j=0; j<o; ++j)
-            for (k=0, result[i][j]=0.0; k<m; ++k)
+    for(i=0; i<n; ++i)
+        for(j=0; j<o; ++j)
+            for(k=0, result[i][j]=0.0; k<m; ++k)
             {
                 result[i][j] += Apinv[i][k] * Y[k][j];
             }
@@ -52,16 +52,16 @@ MATRIX mat_w_least_squares(MATRIX A, MATRIX Y, MATRIX w, MATRIX result)
     W = mat_creat_diag(w);
     if(MatRow(A)!= MatRow(Y)) return mat_error(MAT_SIZEMISMATCH);
     Awpinv = mat_wpinv(A, W, NULL);
-    if (Awpinv == NULL) return mat_error(MAT_INVERSE_ILL_COND);
+    if(Awpinv == NULL) return mat_error(MAT_INVERSE_ILL_COND);
 
     m = MatCol(Awpinv);
     n = MatRow(Awpinv);
     o = MatCol(Y);
-    if (result == NULL) if ((result= mat_creat( n, o, UNDEFINED )) == NULL)
+    if(result == NULL) if((result= mat_creat( n, o, UNDEFINED )) == NULL)
             return mat_error(MAT_MALLOC);
-    for (i=0; i<n; ++i)
-        for (j=0; j<o; ++j)
-            for (k=0, result[i][j]=0.0; k<m; ++k)
+    for(i=0; i<n; ++i)
+        for(j=0; j<o; ++j)
+            for(k=0, result[i][j]=0.0; k<m; ++k)
             {
                 result[i][j] += Awpinv[i][k] * Y[k][j];
             }
@@ -82,7 +82,7 @@ MATRIX mat_rob_least_squares(MATRIX A, MATRIX Y, int lossfunc)
     norm_th = 0.0001f*mat_sum(tmp1);
     mat_free(tmp1);
 
-    for (k = 0; k<MAX_ITERS_RB && flag == 0; ++k)
+    for(k=0; k<MAX_ITERS_RB && flag == 0; ++k)
     {
         X = mat_w_least_squares(A, Y, W, X);
         mat_free(W);
@@ -90,7 +90,7 @@ MATRIX mat_rob_least_squares(MATRIX A, MATRIX Y, int lossfunc)
         tmp1 = mat_mul(A, X, NULL);
         res_ = mat_sub(tmp1, Y, NULL);
         mat_free(tmp1);
-        if (k == 0)
+        if(k==0)
         {
             med = mat_median(res_);
             tmp1 = mat_subs(res_, med,NULL);
@@ -128,9 +128,9 @@ MATRIX mat_robust_fit(MATRIX data, MATRIX Y, int degree, int lossfunc)
     MATRIX A = 0, X = 0;
     n = MatRow(data);
     A = mat_creat(n, degree+1, ONES_MATRIX);
-    for (i = 0; i<n; ++i)
+    for(i=0; i<n; ++i)
     {
-        for (j = degree-1; j>=0; --j) A[i][j] = data[i][0]*A[i][j+1];
+        for(j=degree-1; j>=0; --j) A[i][j] = data[i][0]*A[i][j+1];
     }
     X = mat_rob_least_squares(A, Y, lossfunc);
     mat_free(A);
