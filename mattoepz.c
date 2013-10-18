96f9fd890a4754064a665695a2d2aa8bdda9ec21
Sk Mohammadul Haque <mohammadul@gmail.com>
2013-10-18 21:15:19 +0530

    Update mattoepz.c

--- mattoepz.c
+++ mattoepz.c
@@ -5,14 +5,14 @@
 
 MATRIX mat_symtoeplz(MATRIX R)
 {
-    int	i, j, n;
-    MATRIX	T;
+    int i, j, n;
+    MATRIX T;
 
     n = MatRow(R);
     T = mat_creat(n, n, UNDEFINED);
 
-    for (i=0; i<n; ++i)
-        for (j=0; j<n; ++j)
+    for(i=0; i<n; ++i)
+        for(j=0; j<n; ++j)
         {
             T[i][j] = R[abs(i-j)][0];
         }
