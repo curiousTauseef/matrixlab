33ab9257c676f5b27c85924f30444a2343418a78
Sk Mohammadul Haque <mohammadul@gmail.com>
2013-10-18 22:07:49 +0530

    Update matdatastruct.c

--- matdatastruct.c
+++ matdatastruct.c
@@ -3,7 +3,7 @@
 #include <malloc.h>
 #include "matrix.h"
 
-SEARCH_TREE mat_bs_make_null( void )
+SEARCH_TREE mat_bs_make_null(void)
 {
     return NULL;
 }
@@ -19,63 +19,63 @@ SEARCH_TREE mat_bs_free(SEARCH_TREE T)
     return NULL;
 }
 
-SEARCH_TREE mat_bs_find( mtype x, SEARCH_TREE T )
+SEARCH_TREE mat_bs_find( mtype x, SEARCH_TREE T)
 {
-    if( T == NULL ) return NULL;
-    if( x < T->element ) return( mat_bs_find( x, T->left ) );
-    else if( x > T->element ) return( mat_bs_find( x, T->right ) );
+    if(T==NULL) return NULL;
+    if(x<T->element) return(mat_bs_find(x, T->left));
+    else if(x>T->element) return(mat_bs_find(x, T->right));
     else return T;
 }
 
 SEARCH_TREE mat_bs_find_min( SEARCH_TREE T )
 {
-    if( T != NULL )
-        while( T->left != NULL ) T = T->left;
+    if(T!=NULL)
+        while(T->left!= NULL) T = T->left;
     return T;
 }
 
-SEARCH_TREE mat_bs_find_max( SEARCH_TREE T )
+SEARCH_TREE mat_bs_find_max(SEARCH_TREE T)
 {
-    if( T != NULL )
-        while( T->right != NULL ) T = T->right;
+    if(T!=NULL)
+        while(T->right!=NULL) T = T->right;
     return T;
 }
 
-SEARCH_TREE mat_bs_insert( mtype x, SEARCH_TREE T )
+SEARCH_TREE mat_bs_insert(mtype x, SEARCH_TREE T)
 {
-    if( T == NULL )
+    if(T==NULL)
     {
-        T = (SEARCH_TREE) malloc ( sizeof (struct tree_node) );
-        if( T == NULL ) gen_error(GEN_MALLOC);
+        T = (SEARCH_TREE) malloc (sizeof(struct tree_node));
+        if(T==NULL) gen_error(GEN_MALLOC);
         else
         {
             T->element = x;
             T->left = T->right = NULL;
         }
     }
-    else if( x < T->element ) T->left = mat_bs_insert( x, T->left );
-    else if( x > T->element ) T->right = mat_bs_insert( x, T->right );
+    else if(x<T->element) T->left = mat_bs_insert(x, T->left);
+    else if(x>T->element) T->right = mat_bs_insert(x, T->right);
     return T;
 }
 
 SEARCH_TREE mat_bs_delete( mtype x, SEARCH_TREE T )
 {
     SEARCH_TREE tmp_cell, child = 0;
-    if( T == NULL ) gen_error(GEN_NOT_FOUND);
-    else if( x < T->element ) T->left = mat_bs_delete( x, T->left );
-    else if( x > T->element ) T->right = mat_bs_delete( x, T->right );
-    else if( T->left && T->right )
+    if(T==NULL) gen_error(GEN_NOT_FOUND);
+    else if(x<T->element) T->left = mat_bs_delete(x, T->left);
+    else if(x>T->element) T->right = mat_bs_delete(x, T->right);
+    else if(T->left && T->right)
     {
-        tmp_cell = mat_bs_find_min( T->right );
+        tmp_cell = mat_bs_find_min(T->right);
         T->element = tmp_cell->element;
-        T->right = mat_bs_delete( T->element, T->right );
+        T->right = mat_bs_delete(T->element, T->right);
     }
     else
     {
         tmp_cell = T;
-        if( T->left == NULL ) child = T->right;
-        if( T->right == NULL ) child = T->left;
-        free( tmp_cell );
+        if(T->left == NULL) child = T->right;
+        if(T->right == NULL) child = T->left;
+        free(tmp_cell);
         return child;
     }
     return T;
@@ -96,33 +96,33 @@ int mat_bs_inorder(SEARCH_TREE T, int index, mtype **p_ordered)
 INT_STACK int_stack_creat(void)
 {
     INT_STACK s;
-    if((s= (INT_STACK)malloc( sizeof(struct int_stack) )  ) == NULL) stack_error( STACK_MALLOC);
+    if((s = (INT_STACK)malloc(sizeof(struct int_stack)))==NULL) stack_error(STACK_MALLOC);
     s->p = 0;
     s->length = STACK_MAX;
-    if ((s->stack =   (int*)malloc( sizeof(int) *(STACK_MAX))  ) == NULL) stack_error( STACK_MALLOC);
+    if((s->stack = (int*)malloc(sizeof(int)*(STACK_MAX)))==NULL) stack_error(STACK_MALLOC);
     return s;
 }
 
 int int_stack_free(INT_STACK s)
 {
-    if (s == NULL)  return (0);
+    if (s==NULL) return 0;
     free(s->stack);
-    return (1);
+    return 1;
 }
 
 void int_stack_push(INT_STACK s, int value)
 {
     if(s->p >=s->length)
     {
-        if((s->stack = (int *)realloc(s->stack, sizeof(int) * (s->length + STACK_MAX)))==NULL) stack_error(STACK_MALLOC);
+        if((s->stack = (int *)realloc(s->stack, sizeof(int)*(s->length+STACK_MAX)))==NULL) stack_error(STACK_MALLOC);
         s->length += STACK_MAX;
     }
-    s->stack[s->p++] =  value;
+    s->stack[s->p++] = value;
 }
 
 int int_stack_pop(INT_STACK s)
 {
-    if(s->p > 0 )
+    if(s->p>0)
     {
         return s->stack[(--s->p)];
     }
@@ -137,16 +137,16 @@ int int_stack_is_empty(INT_STACK s)
 MAT_MTYPE_STACK mat_mtype_stack_creat(void)
 {
     MAT_MTYPE_STACK s;
-    if((s= (MAT_MTYPE_STACK)malloc( sizeof(struct mat_mtype_stack)))==NULL) stack_error( STACK_MALLOC);
+    if((s= (MAT_MTYPE_STACK)malloc(sizeof(struct mat_mtype_stack)))==NULL) stack_error( STACK_MALLOC);
     s->p = 0;
     s->length = STACK_MAX;
-    if((s->stack=(mtype*)malloc( sizeof(mtype)*(STACK_MAX)))==NULL) stack_error( STACK_MALLOC);
+    if((s->stack=(mtype*)malloc(sizeof(mtype)*(STACK_MAX)))==NULL) stack_error( STACK_MALLOC);
     return s;
 }
 
 int mat_mtype_stack_free(MAT_MTYPE_STACK s)
 {
-    if (s == NULL)  return (0);
+    if(s==NULL) return 0;
     free(s->stack);
     return (1);
 }
@@ -200,7 +200,7 @@ int int_queue_free(INT_QUEUE s)
         }
         s->tail =NULL;
     }
-    return (1);
+    return 1;
 }
 
 void int_queue_enqueue(INT_QUEUE s, int value)
@@ -226,7 +226,7 @@ int int_queue_dequeue(INT_QUEUE s)
 {
     int  value;
     QINTDATA t;
-    if(s->head == NULL) queue_error(QUEUE_EMPTY);
+    if(s->head==NULL) queue_error(QUEUE_EMPTY);
     t = s->head->next;
     value = s->head->data;
     free(s->head);
@@ -249,7 +249,7 @@ int int_queue_is_empty(INT_QUEUE s)
 MAT_MTYPE_QUEUE mat_mtype_queue_creat(void)
 {
     MAT_MTYPE_QUEUE s;
-    if((s= (MAT_MTYPE_QUEUE)malloc( sizeof(struct mat_mtype_queue))) == NULL) queue_error(QUEUE_MALLOC);
+    if((s = (MAT_MTYPE_QUEUE)malloc( sizeof(struct mat_mtype_queue))) == NULL) queue_error(QUEUE_MALLOC);
     s->p = 0;
     s->head = NULL;
     s->tail = NULL;
@@ -258,7 +258,7 @@ MAT_MTYPE_QUEUE mat_mtype_queue_creat(void)
 
 int mat_mtype_queue_free(MAT_MTYPE_QUEUE s)
 {
-    if (s == NULL)  return (0);
+    if(s == NULL) return 0;
     if(s->head!= NULL)
     {
         QMDATA t;
@@ -268,15 +268,15 @@ int mat_mtype_queue_free(MAT_MTYPE_QUEUE s)
             s->head = s->head->next;
             free(t);
         }
-        s->tail =NULL;
+        s->tail = NULL;
     }
-    return (1);
+    return 1;
 }
 
 void mat_mtype_queue_enqueue(MAT_MTYPE_QUEUE s, mtype value)
 {
     QMDATA t;
-    if ((t =  (QMDATA)malloc( sizeof(qmdata))  ) == NULL) queue_error(QUEUE_MALLOC);
+    if((t = (QMDATA)malloc(sizeof(qmdata)))==NULL) queue_error(QUEUE_MALLOC);
     t->next=NULL;
     t->data = value;
     if(s->head==NULL)
@@ -324,8 +324,8 @@ int mat_mtype_queue_is_empty(MAT_MTYPE_QUEUE s)
 INT_PRIORITYQUEUE int_priorityqueue_creat()
 {
     INT_PRIORITYQUEUE H;
-    if((H = (INT_PRIORITYQUEUE) malloc ( sizeof (int_priorityqueue))) == NULL) pq_error(PQ_MALLOC);
-    if((H->element = (PQINTDATA) malloc( ( STACK_MAX+1) * sizeof (pqintdata)))==NULL ) pq_error(PQ_MALLOC);
+    if((H = (INT_PRIORITYQUEUE) malloc(sizeof(int_priorityqueue)))==NULL) pq_error(PQ_MALLOC);
+    if((H->element = (PQINTDATA) malloc((STACK_MAX+1)*sizeof(pqintdata)))==NULL ) pq_error(PQ_MALLOC);
     H->length = STACK_MAX;
     H->p = 0;
     H->element[0].priority = INT_MAX;
@@ -336,13 +336,13 @@ INT_PRIORITYQUEUE int_priorityqueue_creat()
 void int_priorityqueue_enqueue(INT_PRIORITYQUEUE H, int data, int priority)
 {
     int i;
-    if(H->length == H->p)
+    if(H->length==H->p)
     {
-        if((H->element = (PQINTDATA)realloc(H->element, sizeof(pqintdata) * (H->length + STACK_MAX+1)))==NULL) pq_error(PQ_MALLOC);
+        if((H->element = (PQINTDATA)realloc(H->element, sizeof(pqintdata)*(H->length+STACK_MAX+1)))==NULL) pq_error(PQ_MALLOC);
         H->length += STACK_MAX;
     }
     i = ++H->p;
-    while( H->element[i/2].priority < priority)
+    while(H->element[i/2].priority<priority)
     {
         H->element[i] = H->element[i/2];
         i /= 2;
@@ -377,9 +377,9 @@ int int_priorityqueue_free(INT_PRIORITYQUEUE H)
 {
     if(H==NULL) return 0;
     free(H->element);
-    H->element =NULL;
+    H->element = NULL;
     free(H);
-    return(1);
+    return 1;
 }
 
 int int_priorityqueue_update(INT_PRIORITYQUEUE H, int data, int priority, int type)
@@ -389,13 +389,13 @@ int int_priorityqueue_update(INT_PRIORITYQUEUE H, int data, int priority, int ty
     for(i=1; i<=H->p; ++i) if(H->element[i].data==data) p =i;
     if(p!=0)
     {
-        if((type==0 || type == 2)  && H->element[p].priority>priority)
+        if((type==0 || type==2) && H->element[p].priority>priority)
         {
-            for( i=p; i*2 <= H->p; i=child)
+            for(i=p; (i*2)<= H->p; i=child)
             {
                 child = i*2;
-                if((child != H->p) && ( H->element[child+1].priority < H->element[child].priority ) ) child++;
-                if(priority > H->element[child].priority)  H->element[i] = H->element[child];
+                if((child!=H->p)&&( H->element[child+1].priority<H->element[child].priority)) child++;
+                if(priority>H->element[child].priority)  H->element[i] = H->element[child];
                 else break;
             }
             H->element[i].data = data;
