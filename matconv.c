821a0794417e6f3080e8cd6bc58680f6e93e1990
Sk Mohammadul Haque <mohammadul@gmail.com>
2013-11-15 00:54:02 +0530

    Added mat_vectorize_tr().

--- matconv.c
+++ matconv.c
@@ -12,7 +12,7 @@ INT_VECTOR mat_2int_vec(MATRIX a)
     if((v = int_vec_creat(l, UNDEFINED))==NULL) int_vec_error(INT_VECSTACK_MALLOC);
     l = 0;
     #pragma omp parallel for private(j)
-    for (i=0; i<n; ++i)
+    for(i=0; i<n; ++i)
         for(j=0; j<m; ++j)
         {
             v[l] = (int)a[i][j];
@@ -30,13 +30,13 @@ MATRIX int_vec2_mat(INT_VECTOR a, int direction)
     {
         if((M = mat_creat(n, 1, UNDEFINED))==NULL) mat_error(MAT_MALLOC);
         #pragma omp parallel for
-        for (i=0; i<n; ++i) M[i][0] = a[i];
+        for(i=0; i<n; ++i) M[i][0] = a[i];
     }
     else
     {
         if((M = mat_creat(1, n, UNDEFINED))==NULL) mat_error(MAT_MALLOC);
         #pragma omp parallel for
-        for (i=0; i<n; ++i) M[0][i] =a[i];
+        for(i=0; i<n; ++i) M[0][i] =a[i];
     }
     return M;
 }
@@ -51,10 +51,30 @@ MATRIX mat_vectorize(MATRIX A, MATRIX result)
             return mat_error(MAT_MALLOC);
 
     #pragma omp parallel for private(j)
-    for (i=0; i<n; ++i)
-        for (j=0; j<m; ++j)
+    for(i=0; i<n; ++i)
+        for(j=0; j<m; ++j)
         {
             result[k][0]= A[i][j];
+            ++k;
+        }
+    if(flag) mat_free(A);
+    return (result);
+}
+
+MATRIX mat_vectorize_tr(MATRIX A, MATRIX result)
+{
+    int i, j, m, n, flag = 0, k=0;
+    m = MatCol(A);
+    n = MatRow(A);
+    if(result==A) flag =1;
+    if(result== NULL|| result ==A)if((result = mat_creat(1, MatNumel(A), UNDEFINED )) == NULL)
+            return mat_error(MAT_MALLOC);
+
+    #pragma omp parallel for private(j)
+    for(i=0; i<n; ++i)
+        for(j=0; j<m; ++j)
+        {
+            result[0][k] = A[i][j];
             ++k;
         }
     if(flag) mat_free(A);
