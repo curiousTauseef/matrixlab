32c1e1d0b023004f8b42e0daaa0806fbe180889e
Sk Mohammadul Haque <mohammadul@gmail.com>
2013-11-15 22:43:22 +0530

    Update matdet.c

--- matdet.c
+++ matdet.c
@@ -5,10 +5,10 @@ static mtype signa[2] = {1.0, -1.0};
 
 mtype mat_minor(MATRIX A, int i, int j)
 {
-    MATRIX	S;
+    MATRIX S;
     mtype result;
     S = mat_submat(A, i, j);
-    result = mat_det( S );
+    result = mat_det(S);
     mat_free(S);
     return (result);
 }
@@ -16,7 +16,7 @@ mtype mat_minor(MATRIX A, int i, int j)
 mtype mat_cofact(MATRIX A, int i, int j)
 {
     mtype result;
-    result = signa[(i+j)%2] * A[i][j] * mat_minor(A, i, j);
+    result = signa[(i+j)%2]*A[i][j]*mat_minor(A, i, j);
     return (result);
 }
 
@@ -29,28 +29,16 @@ mtype mat_det(MATRIX a)
     A = mat_copy(a, NULL);
     P = mat_creat(n, 1, UNDEFINED);
 
-    /*
-    * take a LUP-decomposition
-    */
     i = mat_lu(A, P);
     switch (i)
     {
-        /*
-        * case for singular matrix
-        */
     case -1:
         result = 0.0;
         break;
 
-        /*
-        * normal case: |A| = |L||U||P|
-        * |L| = 1,
-        * |U| = multiplication of the diagonal
-        * |P| = +-1
-        */
     default:
         result = 1.0;
-        for (j=0; j<n; ++j)
+        for(j=0; j<n; ++j)
         {
             result *= A[(int)P[j][0]][j];
         }
