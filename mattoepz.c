0f1f5e98b3540de952ed4e498e479330e59125d6
Sk Mohammadul Haque <mohammadul@gmail.com>
2013-11-16 16:16:54 +0530

    Update mattoepz.c

--- mattoepz.c
+++ mattoepz.c
@@ -3,19 +3,16 @@
 #include <math.h>
 #include "matrix.h"
 
-MATRIX mat_symtoeplz(MATRIX R)
+MATRIX mat_symtoeplz(MATRIX R, MATRIX result)
 {
     int i, j, n;
-    MATRIX T;
-
     n = MatRow(R);
-    T = mat_creat(n, n, UNDEFINED);
-
+    if(result==NULL) if((result = mat_creat(n, n, UNDEFINED))==NULL) return mat_error(MAT_MALLOC);
     for(i=0; i<n; ++i)
         for(j=0; j<n; ++j)
         {
-            T[i][j] = R[abs(i-j)][0];
+            result[i][j] = R[abs(i-j)][0];
         }
-    return (T);
+    return result;
 }
 
