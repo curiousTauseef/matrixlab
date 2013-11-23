17220469cf5d64adfe203017e5a787d2f2b67d5d
Sk Mohammadul Haque <mohammadul@gmail.com>
2013-11-23 17:52:31 +0530

    Update matsum.c

--- matsum.c
+++ matsum.c
@@ -3,19 +3,18 @@
 
 mtype mat_sum(MATRIX A)
 {
-    int	i, j, m, n;
+    int i, j, m, n;
     mtype mn = 0.0;
     m = MatCol(A);
     n = MatRow(A);
-
     for (i=0; i<n; ++i)
         for(j=0; j<m; ++j) mn += A[i][j];
-    return (mn);
+    return mn;
 }
 
 MATRIX mat_sum_row(MATRIX A, MATRIX result)
 {
-    int	i, j, m, n;
+    int i, j, m, n;
     m = MatCol(A);
     n = MatRow(A);
     if(result==NULL) if((result = mat_creat(n, 1, ZERO_MATRIX))==NULL) mat_error(MAT_MALLOC);
@@ -27,7 +26,7 @@ MATRIX mat_sum_row(MATRIX A, MATRIX result)
 
 MATRIX mat_sum_col(MATRIX A, MATRIX result)
 {
-    int	i, j, m, n;
+    int i, j, m, n;
     m = MatCol(A);
     n = MatRow(A);
     if(result==NULL) if((result = mat_creat(1, m, ZERO_MATRIX))==NULL) mat_error(MAT_MALLOC);
